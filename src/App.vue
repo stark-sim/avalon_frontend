01deb554e567f34d0a0dacc864aa8591bb43f472
枕哒 <gooda159753@163.com>
2023-01-02 23:21:16 +0800

    feat: add @vue/apollo-composable

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+
+import gql from 'graphql-tag'
+import { useQuery } from '@vue/apollo-composable'
+
+const CHARACTERS_QUERY = gql`
+  query Characters {
+    characters {
+      results {
+        id
+        name
+        image
+      }
+    }
+  }
+`
+const { result, loading, error } = useQuery(CHARACTERS_QUERY);
 </script>
 
 <template>
   <div>
+    <p v-if="error">Something went wrong...</p>
+    <p v-if="loading">Loading...</p>
+    <p v-else v-for="character in result.characters.results" :key="character.id">
+      {{ character.name }}
+    </p>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -20,9 +41,11 @@ import HelloWorld from './components/HelloWorld.vue'
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
