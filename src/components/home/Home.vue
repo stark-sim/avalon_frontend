6e7bc07e4c9882132000d4c4d9827fcffd221a38
shenda <shenda@hqshuke.com>
2023-01-06 13:43:58 +0800

    feat: add MessageBox ui

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -7,6 +7,7 @@ import gql from 'graphql-tag'
 import { useQuery } from '@vue/apollo-composable'
 
 import { ArrowRight } from '@element-plus/icons-vue'
+import { ElMessage, ElMessageBox } from 'element-plus'
 
 import { useRouter } from "vue-router"; // 导入路由
 const router = useRouter() // 实例化路由
@@ -18,19 +19,61 @@ if (userID == null) {
   router.push("/login")
 }
 
+// TODO 检查有没有在某个房间，有的话直接去房间页面
+
 function logout() {
   console.log("logout")
   deleteUserToken()
   router.push("/login")
 }
 
+const openCreateRoom = () => {
+  ElMessageBox({
+    title: '创建房间',
+    showCancelButton: false,
+    confirmButtonText: "创建",
+  }).then(({value}) => {
+    ElMessage({
+      type: "success",
+      message: `创建房间 ${value}`
+    })
+  })
+}
+
+const openJoinRoom = () => {
+  ElMessageBox.prompt('请输入房间号', {
+    confirmButtonText: "加入",
+  }).then(({ value }) => {
+    ElMessage({
+      type: "success",
+      message: `正在加入 ${value} 房间`
+    })
+  }).catch(() => {
+    ElMessage({
+      type: 'info',
+      message: `取消加入`
+    })
+  })
+}
+
+function createRoom() {
+
+}
+
+function joinRoom() {
+
+}
+
 </script>
 
 <template>
   <h1>"主页"</h1>
-  <el-button type="primary" :icon="ArrowRight" @click="">创建房间</el-button>
-  <el-button type="primary" :icon="ArrowRight" @click="">加入房间</el-button>
-  <el-button type="primary" :icon="ArrowRight" @click="logout()">登出</el-button>
+  <div :class="buttonList">
+    <el-button type="primary" :icon="ArrowRight" @click="openCreateRoom">创建房间</el-button>
+    <el-button type="primary" :icon="ArrowRight" @click="openJoinRoom">加入房间</el-button>
+    <el-button type="primary" :icon="ArrowRight" @click="logout()">退出登录</el-button>
+  </div>
+  
 </template>
 
 <style scoped>
